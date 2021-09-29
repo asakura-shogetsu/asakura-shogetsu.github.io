2d0de9710fe1fd50a7bf22ce7b521062c7060037
m_shimao <m_shimao@radius5.co.jp>
2021-09-29 22:02:41 +0900

    お知らせ等更新

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,13 +6,13 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              9月の定休日は8（水）、15（水）、21（火）、27（月）になります。
+              10月の定休日は10（日）、11（月）、20（水）、27（水）になります。
+            </li>
+            <li className="border-dotted border-b-2 border-black my-4">
+              あさくらペイのご利用が可能です。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               新型コロナウイルス感染症拡大防止のため、8月20日〜9月30日の間、酒類の提供は出来ません。また、営業時間は11〜15時・17〜20時となります。
-            </li>
-            <li className="border-dotted border-b-2 border-black my-4">
-              2021年9月4日、ホームページを公開しました。
             </li>
           </ul>
         </div>
