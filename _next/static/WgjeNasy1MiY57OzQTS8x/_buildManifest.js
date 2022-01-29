bd395794630bfd165f7681cfc743140135ac79f8
m-shimao <m-shimao@users.noreply.github.com>
2022-01-29 00:33:41 +0000

    deploy: 5a6522047619351ba735d1d072b1632c67c43527

---
+++ _next/static/WgjeNasy1MiY57OzQTS8x/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/787-f8889ab4b6304b6b.js","static/chunks/pages/index-f0faa5e34c5ff92b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
