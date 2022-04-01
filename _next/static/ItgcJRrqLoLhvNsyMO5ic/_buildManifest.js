eda07bf1687c44c40706229be5a9a46d0a41ffe9
asakura-shogetsu <asakura-shogetsu@users.noreply.github.com>
2022-04-01 13:13:48 +0000

    deploy: e197a4e088baeefbf7663a8f2f7f9f9e1ef0c43c

---
+++ _next/static/ItgcJRrqLoLhvNsyMO5ic/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/787-51474eac8809da56.js","static/chunks/pages/index-5d26e8c199dbc315.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
