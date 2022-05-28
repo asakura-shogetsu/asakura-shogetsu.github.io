df9dea583eb696e1e07009c77e7c2c7f73a59a89
asakura-shogetsu <asakura-shogetsu@users.noreply.github.com>
2022-05-28 07:50:00 +0000

    deploy: 7e5136b87cf680db90496860927aeb5f36100c20

--- _next/static/mDKDBmnnCdAC8iwc-xtfr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/787-72e05b9daaedb08d.js","static/chunks/pages/index-95bf2bc1168459e3.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
