1474710b8e9c1eb741c8d4e7b7bd589a6548bc93
m-shimao <asakura.shogetsu@gmail.com>
2023-03-21 07:10:10 +0900

    update

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,12 +6,12 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              3月の定休日は8（水）、15（水）、22（水）、27（月）になります。<br />
-              うまかけん福岡、避密の旅の電子クーポンのみご利用いただけます。< br />
+              4月の定休日は6（木）、12（水）、21（金）、26（水）です。5月のゴールデンウィークは、1（月）のみ休業いたします。<br />
+              うまかけん福岡、避密の旅の電子クーポンのみご利用いただけます。テイクアウトもお尋ねください。< br />
               蒸し雑煮・鰻しゃぶは、3月までの冬メニューです。ご来店の際は、ぜひご賞味ください。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
-              2月の定休日は1（水）、8（水）、15（水）、22（水）になります。
+              3月の定休日は22（水）、27（月）です。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               メニュー・金額に一部変更がございます。ご確認ください。
