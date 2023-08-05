aa53472c6ef8146e0ecef18fcf3dcd3602d05453
m-shimao <shimao.ma@gmail.com>
2023-08-05 15:32:25 +0900

    お知らせ更新

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,11 +6,12 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              8月の定休日は3（木）、8（火）、15（火）、24（木）、30（水）、31（木）です（お盆期間中の定休日は15日のみです）。<br />
-              朝倉振興券、朝倉ペイのご利用が可能です。
+              7/30のももち浜ストアにて当店が紹介されました。<br />
+              8/1より、一部料金の変更がございます。詳細は店舗メニューにてご確認ください（本ページの料金は追って修正予定です）。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
-              7月の定休日は27（木）です。7月30日（日）は「土用の丑の日」です。スタッフ一同ご来店をお待ちしております。
+              8月の定休日は3（木）、8（火）、15（火）、24（木）、30（水）、31（木）です（お盆期間中の定休日は15日のみです）。<br />
+              朝倉振興券、朝倉ペイのご利用が可能です。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               メニュー・金額に一部変更がございます。ご確認ください。
