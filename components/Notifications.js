a8ff08b2a8868decd3ce1b938a0462062ef5e073
m-shimao <asakura.shogetsu@gmail.com>
2023-10-18 16:44:50 +0900

    お知らせ更新

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,11 +6,13 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              10月の定休日は3（火）、12（木）、22（日）、23（月）です。<br />
+              11月の定休日は6（月）、15（水）、23（木）、29（水）です。<br />
+              11月より、鍋料理・蒸し雑煮が始まります。<br />
+              大将がバイク好きのため、バイクで来店されたお客様に限りコーヒー1杯無料サービスをしています。お気軽にお声掛けください。<br />
               朝倉振興券、朝倉ペイのご利用が可能です。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
-              大将がバイク好きのため、バイクで来店されたお客様に限りコーヒー1杯サービスを始めました。お気軽にお声掛けください。
+              10月の定休日は22（日）、23（月）です。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               7/31のももち浜ストアにて当店が紹介されました。
