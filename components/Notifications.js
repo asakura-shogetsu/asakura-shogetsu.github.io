12d6dc04dc9e82b3aaa59d0b7fc0f26ac01ce35d
m-shimao <shimao.ma@gmail.com>
2024-02-20 13:26:09 +0900

    お知らせ更新

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,10 +6,10 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              2月の店休日は8（木）、21（水）、28（水）です。また、14（水）・15（木）はランチ時間のみの営業です。よろしくお願いいたします。
+              3月の店休日は7（木）、14（木）、21（木）、27（水）です。冬限定の鍋料理・蒸し雑煮は3月で終了します。次回は11月からです。よろしくお願いします。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
-              1月の店休日は31（水）です。
+              2月の店休日は21（水）、28（水）です。よろしくお願いいたします。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               ご来店の際は、お電話にて席の空き状況をお確かめください。状況次第でお待たせしてしまう場合がございます。
