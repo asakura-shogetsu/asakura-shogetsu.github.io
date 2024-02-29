fd39e796b7f57a0f141f8f59afa48f458e631c7c
m-shimao <asakura.shogetsu@gmail.com>
2024-02-29 21:01:37 +0900

    3/20追加」

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,7 +6,7 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              3月の店休日は7（木）、14（木）、21（木）、27（水）です。冬限定の鍋料理・蒸し雑煮は3月で終了します。次回は11月からです。よろしくお願いします。
+              3月の店休日は7（木）、14（木）、20（水）、21（木）、27（水）です。冬限定の鍋料理・蒸し雑煮は3月で終了します。次回は11月からです。よろしくお願いします。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               2月の店休日は21（水）、28（水）です。よろしくお願いいたします。
