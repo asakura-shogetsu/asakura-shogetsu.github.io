604a0e3babe95daa71c96e22ec5c4974e2841c3b
m-shimao <asakura.shogetsu@gmail.com>
2024-04-10 12:13:24 +0900

    お知らせ更新

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,11 +6,11 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              4月の店休日は3（水）、11（木）、17（水）の夜、18（木）、25（木）です。<br />
-              ゴールデンウィーク中は休まずに営業いたします。クレジット払い・スマホ決済可能です。ご来店をお待ちしております。
+              5月の店休日は7（火）、8（水）、16（木）、22（水）、29（水）です。<br />
+              ゴールデンウィーク中は休まずに営業いたします。クレジット払い・スマホ決済可能です。皆様のご来店をお待ちしております。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
-              3月の店休日は21（木）、27（水）です。冬限定の鍋料理・蒸し雑煮は3月で終了します。次回は11月からです。よろしくお願いします。
+              4月の店休日は11（木）、17（水）の夜、18（木）、25（木）です。<br />
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               ご来店の際は、お電話にて席の空き状況をお確かめください。状況次第でお待たせしてしまう場合がございます。
