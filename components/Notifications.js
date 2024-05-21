018f54125fa3358aa082831909e278634afefc77
m-shimao <asakura.shogetsu@gmail.com>
2024-05-21 20:02:32 +0900

    お知らせ更新

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,20 +6,20 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              5月の店休日は7（火）、8（水）、16（木）、22（水）、29（水）です。<br />
-              ゴールデンウィーク中は休まずに営業いたします。クレジット払い・スマホ決済可能です。皆様のご来店をお待ちしております。
+              6月の店休日は5（水）午後（昼のみ営業）、6（木）、12（水）、19（水）、27（木）です。<br />
+              クレジット払い・スマホ決済可能です。皆様のご来店をお待ちしております。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
-              4月の店休日は11（木）、17（水）の夜、18（木）、25（木）です。<br />
-            </li>
-            <li className="border-dotted border-b-2 border-black my-4">
-              冬限定の鍋料理・蒸し雑煮は3月で終了します。次回は11月からです。よろしくお願いします。
+              5月の店休日は22（水）、29（水）です。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               ご来店の際は、お電話にて席の空き状況をお確かめください。状況次第でお待たせしてしまう場合がございます。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               大将がバイク好きのため、バイクでお越しのお客様に限りコーヒー1杯無料サービスをしております、ぜひご来店お待ちしております。
+            </li>
+            <li className="border-dotted border-b-2 border-black my-4">
+              冬限定の鍋料理・蒸し雑煮は3月で終了しました。次回は11月からです。よろしくお願いします。
             </li>
           </ul>
         </div>
