128bc30222df793d21bd8de7e863b4c0297fb229
m-shimao <asakura.shogetsu@gmail.com>
2024-07-30 16:02:17 +0900

    お知らせ更新

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,11 +6,14 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              7月の店休日は3（水）、10（水）、18（木）、25（木）、31（水）です。<br />
-              クレジット払い・スマホ決済可能です。皆様のご来店をお待ちしております。
+              8月の店休日は7（水）、15（木）、21（水）、22（木）、28（水）です。<br />
+              朝倉振興券・朝倉ペイ、利用可能です。皆様のご来店をお待ちしております。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
-              今年の土用の丑の日は、7月24日（水）・8月5日（月）の2回です。ご来店をお待ちしております。
+              今年2回目の土用の丑の日は、8月5日（月）です。ご来店をお待ちしております。
+            </li>
+            <li className="border-dotted border-b-2 border-black my-4">
+              7月の店休日は31（水）です。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               <a href="#media" className="text-black">雑誌「ソワニエ+ vol.86 2024年7・8月号」に当店が紹介されております。ぜひご覧ください。</a>
