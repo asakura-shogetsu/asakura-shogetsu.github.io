4852c4a9bcc784b54d9ad507284c2318614bebab
asakura-shogetsu <asakura-shogetsu@users.noreply.github.com>
2025-05-21 12:25:19 +0000

    deploy: ce5fc0b073db2e9d45285c6168fcc4d3eecc79d8

---
+++ _next/static/KkpN19bzq6OyQ71NiaYjJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/35-409483c354a4b112.js","static/chunks/pages/index-c48db061fc3138c0.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
