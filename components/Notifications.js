b690ff09be6376ca332a535fa3a640e68f67b3a2
m-shimao <asakura.shogetsu@gmail.com>
2025-07-23 14:50:22 +0900

    お知らせ更新

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,7 +6,10 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              7月の店休日は2（水）、9（水）、16（水）、22（火）、29（火）です。
+              8月の店休日は7（木）、15（金）、18（月）、19（火）、27（水）です。15、18、19はお盆休みです。<br />朝倉振興券が始まりましたが、朝倉ペイは準備中ですので事前の確認をお願いします。朝倉振興券はご利用可能です。
+            </li>
+            <li className="border-dotted border-b-2 border-black my-4">
+              7月の店休日は29（火）です。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               ご来店の際は、お電話にて席の空き状況をお確かめください。状況次第でお待たせしてしまう場合がございます。
