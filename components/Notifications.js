bf8fa9b06f86111f09ee22abe5670105f5f6cfb2
m-shimao <asakura.shogetsu@gmail.com>
2025-09-29 20:30:26 +0900

    お知らせ更新

--- components/Notifications.js
+++ components/Notifications.js
@@ -5,14 +5,11 @@ export default function Notifications() {
       <div className='flex justify-center flex-wrap'>
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
+            <li className="border-dotted border-b-2 border-black my-4">
+              10月の店休日は1（水）、8（水）、14（火）の夜（昼のみ営業）、15（水）、23（木）、29（水）です。<br />朝倉振興券・朝倉ペイ・各種クレジット、ご利用可能です。
+            </li>
             <li className="border-dotted border-b-2 border-black my-4 text-red-500 font-bold">
               鰻の刺身・洗いは特別な処理が必要なため、予約をお願いします。
-            </li>
-            <li className="border-dotted border-b-2 border-black my-4">
-              9月の店休日は3（水）、10（水）、17（水）、24（水）の夜（昼のみ営業）、25（木）です。<br />朝倉振興券・朝倉ペイ・各種クレジット、ご利用可能です。
-            </li>
-            <li className="border-dotted border-b-2 border-black my-4">
-              8月の店休日は18（月）、19（火）、27（水）です。18、19はお盆休みです。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
               ご来店の際は、お電話にて席の空き状況をお確かめください。状況次第でお待たせしてしまう場合がございます。
