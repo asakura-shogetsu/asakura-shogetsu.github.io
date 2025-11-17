29f7bbb46a2bc00b169889a7e35686c760844124
m-shimao <asakura.shogetsu@gmail.com>
2025-11-17 19:23:33 +0900

    お知らせ更新

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,10 +6,11 @@ export default function Notifications() {
         <div className='notifications-box w-full lg:w-1/2'>
           <ul className="list-none px-4 py-2">
             <li className="border-dotted border-b-2 border-black my-4">
-              11月の店休日は5（水）、12（水）、19（水）、26（水）の夜（昼のみ営業）、27（木）です。<br />朝倉振興券・朝倉ペイ・各種クレジット、ご利用可能です。
+              物価高騰のため、鍋料理の値段をやむを得ず1割値上げさせていただきます。大変ご迷惑をお掛けいたしますが、ご理解いただきますようお願いいたします。<br />
+              ※ホームページのメニュー表の価格は価格変更前の表示となっております。追って更新を予定しております。
             </li>
             <li className="border-dotted border-b-2 border-black my-4">
-              10月の店休日は23（木）、29（水）です。
+              11月の店休日は19（水）、26（水）の夜（昼のみ営業）、27（木）です。<br />朝倉振興券・朝倉ペイ・各種クレジット、ご利用可能です。
             </li>
             <li className="border-dotted border-b-2 border-black my-4 text-red-500 font-bold">
               鰻の刺身・洗いは特別な処理が必要なため、予約をお願いします。
